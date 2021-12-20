590de114c27069913780a2e10e4a14669ee8c773
Sadettin048 <96432923+Sadettin048@users.noreply.github.com>
2021-12-20 18:16:36 +0300

    Add files via upload

---
+++ 1 -n sayıya kadar asalsayıyı bulma.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int i,c,asal,n;
+    printf("Bir sayi giriniz:");
+    scanf("%d",&n);
+    printf("1 ile %d arasindaki asal sayilar:\n",n);
+    for(i=2 ; i<=n; i++)
+    {
+        asal=1;
+        for(c=2; c<i; c++)
+        {
+            if(i%c==0)
+            {
+                asal=0;
+            }
+        }
+        if(asal==1)
+            {
+                printf("%d\n",i);
+            }
+        }
+
+    return 0;
+}
